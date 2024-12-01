f5ada4f70d83f1de30e5f63f666053df74c92c84
yuhonakayama <yuhoimaizumi@gmail.com>
2024-12-01 16:11:59 +0900

    kadai_010をアップロード

---
+++ kadai_010/summary.js
@@ -0,0 +1,17 @@
+$(function () {
+  $('#change-color').on('click', function() {
+    $('#target').css('color', 'red');
+  });
+
+  $('#change-text').on('click', function() {
+    $('#target').text('Hello');
+  });
+
+  $('#fade-out').on('click', function() {
+    $('#target').fadeOut(3000);
+  });
+  
+  $('#fade-in').on('click', function() {
+    $('#target').fadeIn(3000);
+  });
+});
